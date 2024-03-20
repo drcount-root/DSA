4c4de5d08df4909343bbb0245b66625421560f80
Subham Sahu <about.subhamsahu@gmail.com>
2024-03-20 23:26:27 +0530

    .

--- two_pointer/merge_sorted_arrays_in_non_decreasing_order/merge_sorted_arrays_in_non_decreasing_order.js
+++ two_pointer/merge_sorted_arrays_in_non_decreasing_order/merge_sorted_arrays_in_non_decreasing_order.js
@@ -1,4 +1,4 @@
-// Leetcode 88 - Merge Sorted Array
+// Leetcode 88 - Merge Sorted Arrays
 
 // You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
