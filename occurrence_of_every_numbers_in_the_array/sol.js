b1de7ea50084f32c2b9bcecf14239ab6e60735f5
Subham Sahu <about.subhamsahu@gmail.com>
2024-07-15 11:54:40 +0530

    .

---
+++ occurrence_of_every_numbers_in_the_array/sol.js
@@ -0,0 +1,22 @@
+const occurrenceFinderFn = (arr) => {
+  const hashMap = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    let getValue = hashMap.get(arr[i]);
+    if (getValue) {
+      getValue++;
+      hashMap.set(arr[i], getValue);
+    } else {
+      hashMap.set(arr[i], 1);
+    }
+  }
+
+  return hashMap;
+};
+
+console.log(occurrenceFinderFn([1, 3, 4, 1, 2, 6, 8, 3, 4, 1, 2, 8]));
+
+// Test cases
+console.log(occurrenceFinderFn([])); // Expected output: Map {}
+console.log(occurrenceFinderFn([1, 2, 3, 4, 5])); // Expected output: Map {1 => 1, 2 => 1, 3 => 1, 4 => 1, 5 => 1}
+console.log(occurrenceFinderFn([1, 2, 2, 3, 3, 3, 4, 4, 4, 4])); // Expected output: Map {1 => 1, 2 => 2, 3 => 3, 4 => 4}
