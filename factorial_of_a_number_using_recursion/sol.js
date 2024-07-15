ed605d4497c3763a50a89d231f2459a7d2a76b0c
Subham Sahu <about.subhamsahu@gmail.com>
2024-07-15 11:19:50 +0530

    .

---
+++ factorial_of_a_number_using_recursion/sol.js
@@ -0,0 +1,14 @@
+const factorialFn = (num) => {
+  let fact = 1;
+
+  if (num > 1) {
+    fact = num * factorialFn(num - 1);
+  }
+
+  return fact;
+};
+
+console.log(factorialFn(5)); // 120
+console.log(factorialFn(0)); // 1
+console.log(factorialFn(1)); // 1
+console.log(factorialFn(2)); // 2
