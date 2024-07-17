fdc656288db9774c0931c1c3e1a5b30311628a77
Subham Sahu <about.subhamsahu@gmail.com>
2024-07-17 15:41:49 +0530

    .

--- TOPICS/recursion/fibonacci_series_up_to_nth_term_using_recursion.js
+++ TOPICS/recursion/fibonacci_series_up_to_nth_term_using_recursion.js
@@ -1,7 +1,38 @@
 // Fibonacci series upto n-th term using recursion
 
-const fibonacciFn = (n, res) => {
-    
-}
+const fibonacciFnRec = (n, f0, f1) => {
+  if (f0 === 0 && f1 === 1) {
+    console.log(f0);
+    console.log(f1);
+  }
 
-// 0 1 1 2 3 5 8 13 21 34
+  if (n > 1) {
+    let fn = f0 + f1;
+    console.log(fn);
+    fibonacciFnRec(n - 1, f1, fn);
+  }
+};
+
+console.log(fibonacciFnRec(10, 0, 1));
+
+// Fibonacci series upto n-th term without using recursion
+
+// const fibonacciFn = (n) => {
+//   let f0 = 0;
+//   let f1 = 1;
+
+//   console.log(f0);
+//   console.log(f1);
+
+//   for (let i = 2; i <= n; i++) {
+//     let fn = f0 + f1;
+//     f0 = f1;
+//     f1 = fn;
+//     console.log(fn);
+//   }
+// };
+
+// console.log(fibonacciFn(10));
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
