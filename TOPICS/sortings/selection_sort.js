e7ac69009a001b715d1519faf88c47df191b7051
Subham Sahu <about.subhamsahu@gmail.com>
2024-07-17 17:58:47 +0530

    .

--- TOPICS/sortings/selection_sort.js
+++ TOPICS/sortings/selection_sort.js
@@ -1,5 +1,22 @@
-const stableSelectionSort = (arr) => {
-  return arr.sort((a,b) => a-b)
+// const stableSelectionSort = (arr) => {
+//   return arr.sort((a,b) => a-b)
+// };
+
+// console.log(stableSelectionSort([3, 7, -4, 5, 2, 1, 9, 0]));
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
 };
 
-console.log(stableSelectionSort([3, 7, -4, 5, 2, 1, 9, 0]));
+console.log(selectionSort([3, 7, -4, 5, 2, 1, 9, 0]));
